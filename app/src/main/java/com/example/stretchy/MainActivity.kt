4ff4718f3c4137944f5f5d54787245fbd2148de6
DGojka <93155190+DGojka@users.noreply.github.com>
2022-10-23 13:34:37 +0200

    Adding dependencies and simple code in MainActivity (#7)
    * Adding dependencies and simple code in MainActivity
    
    * removing unnecessary files

--- app/src/main/java/com/example/stretchy/MainActivity.kt
+++ app/src/main/java/com/example/stretchy/MainActivity.kt
@@ -1,7 +1,42 @@
 package com.example.stretchy
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            com.example.stretchy.ui.theme.StretchyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting2("Android")
+                }
+            }
+        }
+    }
+}
 
+@Composable
+fun Greeting2(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    com.example.stretchy.ui.theme.StretchyTheme {
+        Greeting2("Android")
+    }
 }
