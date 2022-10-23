f4da21d468231dd0568e6eac9cef113d73542ecd
DGojka <dawidgojka4@gmail.com>
2022-10-23 13:27:05 +0200

    removing unnecessary files

--- app/src/main/java/com/example/stretchy/MainActivity.kt
+++ app/src/main/java/com/example/stretchy/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            com.example.stretchy.ui.theme.ui.theme.StretchyTheme {
+            com.example.stretchy.ui.theme.StretchyTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -36,7 +36,7 @@ fun Greeting2(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    com.example.stretchy.ui.theme.ui.theme.StretchyTheme {
+    com.example.stretchy.ui.theme.StretchyTheme {
         Greeting2("Android")
     }
 }
