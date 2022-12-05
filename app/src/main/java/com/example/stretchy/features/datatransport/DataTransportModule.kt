ca5bcc8eedf9c58e94c25f9a387740e1648a6af0
DGojka <dawidgojka4@gmail.com>
2022-12-05 16:10:00 +0100

    Adding Singleton scope

--- app/src/main/java/com/example/stretchy/features/datatransport/DataTransportModule.kt
+++ app/src/main/java/com/example/stretchy/features/datatransport/DataTransportModule.kt
@@ -1,14 +1,18 @@
 package com.example.stretchy.features.datatransport
 
+import com.example.stretchy.features.traininglist.di.TrainingListScope
 import com.example.stretchy.repository.Repository
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class DataTransportModule {
+    @TrainingListScope
     @Provides
     fun provideDataImporter(repository: Repository) = DataImporterImporterImpl(repository)
 
+    @TrainingListScope
     @Provides
     fun provideDataExporter(repository: Repository) = DataExporterExporterImpl(repository)
 }
