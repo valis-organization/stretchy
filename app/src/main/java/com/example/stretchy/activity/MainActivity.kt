57de7a020f89e5683fd87caf51d4b3cfdd82cacb
DGojka <93155190+DGojka@users.noreply.github.com>
2023-09-12 19:53:30 +0200

    bottom navigation bar (#36)
    * bottom navigation bar
    
    * Adding icons

--- app/src/main/java/com/example/stretchy/activity/MainActivity.kt
+++ app/src/main/java/com/example/stretchy/activity/MainActivity.kt
@@ -10,10 +10,22 @@ import android.os.Bundle
 import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.app.ActivityCompat
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.stretchy.BottomNavScreen
 import com.example.stretchy.Navigation
+import com.example.stretchy.R
 import com.example.stretchy.activity.di.ActivityComponent
 
 
@@ -24,15 +36,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         activityComponent = ActivityComponent.create(this)
         setContent {
-            Navigation(
-                activityComponent,
-                {
-                    grantPermissions(WRITE_EXTERNAL_STORAGE)
-                },
-                {
-                    grantPermissions(READ_EXTERNAL_STORAGE)
-                }
-            )
+            BottomNavigationBar()
         }
     }
 
@@ -65,6 +69,74 @@ class MainActivity : ComponentActivity() {
             )
         }
     }
+
+    @Composable
+    fun BottomNavigationBar() {
+        val navController = rememberNavController()
+
+        val screens = listOf(
+            BottomNavScreen(
+                stringResource(id = R.string.stretching),
+                painterResource(id = R.drawable.ic_stretching),
+                stringResource(id = R.string.stretching)
+            ),
+            BottomNavScreen(
+                stringResource(id = R.string.meta_training),
+                painterResource(id = R.drawable.ic_meta_training),
+                stringResource(id = R.string.meta_training)
+            ),
+            BottomNavScreen(
+                stringResource(id = R.string.training),
+                painterResource(id = R.drawable.ic_training),
+                stringResource(id = R.string.training)
+            ),
+        )
+
+        Scaffold(
+            bottomBar = {
+                BottomNavigation {
+                    val navBackStackEntry by navController.currentBackStackEntryAsState()
+                    val currentRoute = navBackStackEntry?.destination?.route
+
+                    screens.forEach { screen ->
+                        BottomNavigationItem(
+                            icon = { Icon(screen.icon, contentDescription = "") },
+                            label = { Text(text = screen.label) },
+                            selected = currentRoute == screen.route,
+                            onClick = {
+                                navController.navigate(screen.route)
+                            }
+                        )
+                    }
+                }
+            }
+        ) { innerPadding ->
+            NavHost(
+                navController = navController,
+                startDestination = stringResource(id = R.string.stretching),
+                modifier = Modifier.padding(innerPadding)
+            ) {
+                composable(getString(R.string.stretching)) {
+                    Navigation(
+                        activityComponent,
+                        {
+                            grantPermissions(WRITE_EXTERNAL_STORAGE)
+                        },
+                        {
+                            grantPermissions(READ_EXTERNAL_STORAGE)
+                        }
+                    )
+                }
+                composable(getString(R.string.meta_training)) {
+
+                }
+                composable(getString(R.string.training)) {
+
+                }
+            }
+        }
+    }
 }
+
 
 
